25184c09e623b0e14a1ca80a80f1658eda399648
Michael Lajlev <lajlev@gmail.com>
2018-09-04 16:42:33 +0200

    Feature

--- index.js
+++ index.js
@@ -15,6 +15,14 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
+  app.get('/times', (req, res) => {
+    let result = ''
+    const times = process.env.TIMES || 5
+    for (i = 0; i < times; i++) {
+      result += i + ' '
+    }
+    res.send(result)
+  })
   .get('/db', async (req, res) => {
     try {
       const client = await pool.connect()
